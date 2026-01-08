080099e669fa0d56fb9544eb7bc7aa0c97f6b356
Meheraj <maharajmd7@gmail.com>
2026-01-09 02:27:33 +0600

    Fix syntax error: remove duplicate closing parentheses in Roadmap

--- frontend/components/Roadmap.tsx
+++ frontend/components/Roadmap.tsx
@@ -324,7 +324,6 @@ export default function Roadmap() {
                                 )}
                             </div>
                         ))}
-                        ))}
                     </div>
                 </div>
             </div>
