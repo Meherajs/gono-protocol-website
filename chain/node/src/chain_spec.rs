6826e7cd5cea05c3b0ed254d55388497f2b2a172
Meheraj <maharajmd7@gmail.com>
2026-08-26 23:14:56 +0600

    style: format chain/node codebase with cargo fmt

--- chain/node/src/chain_spec.rs
+++ chain/node/src/chain_spec.rs
@@ -34,7 +34,9 @@ pub fn development_chain_spec() -> ChainSpec {
 
 	ChainSpec::builder(
 		runtime::WASM_BINARY.expect("WASM binary was not built, please build it!"),
-		Extensions { relay_chain: RELAY_CHAIN.into() },
+		Extensions {
+			relay_chain: RELAY_CHAIN.into(),
+		},
 	)
 	.with_name("Development")
 	.with_id("dev")
@@ -53,7 +55,9 @@ pub fn local_chain_spec() -> ChainSpec {
 
 	ChainSpec::builder(
 		runtime::WASM_BINARY.expect("WASM binary was not built, please build it!"),
-		Extensions { relay_chain: RELAY_CHAIN.into() },
+		Extensions {
+			relay_chain: RELAY_CHAIN.into(),
+		},
 	)
 	.with_name("Local Testnet")
 	.with_id("local_testnet")
